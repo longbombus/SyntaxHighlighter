afd26d8aaebeafccae7177c452644164cfe91817
Angelina <angelinadoroganova@gmail.com>
2021-05-05 21:28:47 +0700

    Дополнение
    Новые стили

--- Styles.js
+++ Styles.js
@@ -55,6 +55,148 @@ const codeStyles = Object.freeze({
     'bold': {bold: true},
     'italic': {italic: true},
   },
+  coy: {
+    _backgroundColor: incapsulateColor(paletteCoy.Background, 'color'), //фон
+    _default: {foregroundColor: incapsulateColor(paletteCoy.Foreground)},// Цвет текста по умолчанию
+
+    'comment': {foregroundColor: incapsulateColor(paletteCoy.Gray)},// комментарии
+    'prolog': 'comment',
+    'doctype': 'comment',
+    'cdata': 'comment',
+    
+    'punctuation': {foregroundColor: incapsulateColor(paletteCoy.GrayLight)}, //Пунктуационные знаки
+    
+    'property': {foregroundColor: incapsulateColor(paletteCoy.Red)},//??
+    
+    'boolean': 'number',
+    'number': {foregroundColor: incapsulateColor(paletteCoy.Red)},//цифры черные
+    'constant': 'number',
+    'symbol': 'number',
+    'deleted': {foregroundColor: incapsulateColor(paletteCoy.Red)},//??
+
+    'selector': 'string',
+    'attr-name': {foregroundColor: incapsulateColor(paletteCoy.Green)},//??
+    'string': {foregroundColor: incapsulateColor(paletteCoy.Green)},// То, что в одинарных кавычках пишется
+    
+    'char': 'number',
+    'builtin': 'string',
+    'inserted': {foregroundColor: incapsulateColor(paletteCoy.Green)},//??
+    
+
+    'operator': {foregroundColor: incapsulateColor(paletteCoy.Brown)}, //равно, или, и 
+    'entity': 'operator',
+    'url': {foregroundColor: incapsulateColor(paletteCoy.Brown), underline: true}, //??
+    
+    'atrule': 'operator',
+    'attr-value': {foregroundColor: incapsulateColor(paletteCoy.Blue)},
+    'keyword': {foregroundColor: incapsulateColor(paletteCoy.Blue)}, // if for и всякие ключевые слова
+
+    'function': {foregroundColor: incapsulateColor(paletteCoy.Green)}, //  названия функций 
+    'class-name': {foregroundColor: incapsulateColor(paletteCoy.Blue), italic: true}, //??
+    'namespace': {foregroundColor: incapsulateColor(paletteCoy.Green), bold: true}, //??
+    'return-type': 'class-name',
+
+    'regex': {foregroundColor: incapsulateColor(paletteCoy.Orange)}, //??
+    'important': 'variable',
+    'variable': {foregroundColor: incapsulateColor(paletteCoy.Brown)}, //???
+
+    'bold': {bold: true},
+    'italic': {italic: true},
+  },
+  default: {
+    _backgroundColor: incapsulateColor(paletteMonokai.Foreground, 'color'), //фон
+    _default: {foregroundColor: incapsulateColor(paletteMonokai.Background)},// Цвет текста по умолчанию
+
+    'comment': {foregroundColor: incapsulateColor(paletteMonokai.Gray)},// комментарии
+    'prolog': 'comment',
+    'doctype': 'comment',
+    'cdata': 'comment',
+    
+    'punctuation': {foregroundColor: incapsulateColor(paletteMonokai.GrayLight)}, //Пунктуационные знаки
+    
+    'property': {foregroundColor: incapsulateColor(paletteMonokai.Blue)},//??
+    
+    'boolean': 'number',
+    'number': {foregroundColor: incapsulateColor(paletteMonokai.Background)},//цифры черные
+    'constant': 'number',
+    'symbol': 'number',
+    'deleted': {foregroundColor: incapsulateColor(paletteMonokai.Red)},//??
+
+    'selector': 'string',
+    'attr-name': {foregroundColor: incapsulateColor(paletteMonokai.Green)},//??
+    'string': {foregroundColor: incapsulateColor(paletteMonokai.Green)},// То, что в одинарных кавычках пишется
+    
+    'char': 'number',
+    'builtin': 'string',
+    'inserted': {foregroundColor: incapsulateColor(paletteMonokai.Green)},//??
+    
+
+    'operator': {foregroundColor: incapsulateColor(paletteMonokai.Gray)}, //равно, или, и 
+    'entity': 'operator',
+    'url': {foregroundColor: incapsulateColor(paletteMonokai.Blue), underline: true}, //??
+    
+    'atrule': 'operator',
+    'attr-value': {foregroundColor: incapsulateColor(paletteMonokai.Blue)},
+    'keyword': {foregroundColor: incapsulateColor(paletteMonokai.Red)}, // if for и всякие ключевые слова
+
+    'function': {foregroundColor: incapsulateColor(paletteMonokai.Green)}, //  названия функций 
+    'class-name': {foregroundColor: incapsulateColor(paletteMonokai.Red), italic: true}, //??
+    'namespace': {foregroundColor: incapsulateColor(paletteMonokai.Green), bold: true}, //??
+    'return-type': 'class-name',
+
+    'regex': {foregroundColor: incapsulateColor(paletteMonokai.Whitish)}, //??
+    'important': 'variable',
+    'variable': {foregroundColor: incapsulateColor(paletteMonokai.OrangeLight)}, //???
+
+    'bold': {bold: true},
+    'italic': {italic: true},
+  },
+  TomorrowNight: {
+    _backgroundColor: incapsulateColor(paletteMonokai.Background, 'color'),
+    _default: {foregroundColor: incapsulateColor(paletteMonokai.Foreground)},
+
+    'comment': {foregroundColor: incapsulateColor(paletteMonokai.Gray)},
+    'prolog': 'comment',
+    'doctype': 'comment',
+    'cdata': 'comment',
+    
+    'punctuation': {foregroundColor: incapsulateColor(paletteMonokai.GrayLight)},
+    
+    'property': {foregroundColor: incapsulateColor(paletteMonokai.Blue)},
+    'tag': {foregroundColor: incapsulateColor(paletteMonokai.Red)},
+    'boolean': 'number',
+    'number': {foregroundColor: incapsulateColor(paletteMonokai.Purple)},
+    'constant': 'number',
+    'symbol': 'number',
+    'deleted': {foregroundColor: incapsulateColor(paletteMonokai.Red)},
+
+    'selector': 'string',
+    'attr-name': {foregroundColor: incapsulateColor(paletteMonokai.Green)},
+    'string': {foregroundColor: incapsulateColor(paletteMonokai.Green)},
+    'char': 'number',
+    'builtin': 'string',
+    'inserted': {foregroundColor: incapsulateColor(paletteMonokai.Green)},
+
+    'operator': {foregroundColor: incapsulateColor(paletteMonokai.Blue)},
+    'entity': 'operator',
+    'url': {foregroundColor: incapsulateColor(paletteMonokai.Blue), underline: true},
+    
+    'atrule': 'operator',
+    'attr-value': {foregroundColor: incapsulateColor(paletteMonokai.Yellow)},
+    'keyword': {foregroundColor: incapsulateColor(paletteMonokai.Purple)},
+
+    'function': {foregroundColor: incapsulateColor(paletteMonokai.Green)},
+    'class-name': {foregroundColor: incapsulateColor(paletteMonokai.Green), italic: true},
+    'namespace': {foregroundColor: incapsulateColor(paletteMonokai.Green), bold: true},
+    'return-type': 'class-name',
+
+    'regex': {foregroundColor: incapsulateColor(paletteMonokai.Whitish)},
+    'important': 'variable',
+    'variable': {foregroundColor: incapsulateColor(paletteMonokai.OrangeLight)},
+
+    'bold': {bold: true},
+    'italic': {italic: true},
+  },
 
   adaptive: {}
 });
